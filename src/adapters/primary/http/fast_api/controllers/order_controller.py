2c2d1e2da9103e466a049d1ca6c408df855e5b36
¨Fran <narfware@gmail.com>
2023-02-09 16:42:38 +0100

    feat(instrument): instrument code with opentelemetry and export tracing to jaeger

--- src/adapters/primary/http/fast_api/controllers/order_controller.py
+++ src/adapters/primary/http/fast_api/controllers/order_controller.py
@@ -1,20 +1,24 @@
 import logging
 from fastapi import status
-
+from fastapi import FastAPI
 from src.adapters.primary.dtos.order_creation import Order_Creation
-
+from src.use_cases.order.create_order.main import Create_Order
+from src.adapters.secondary.tracing.open_telemetry.decorator import instrument
+from src.adapters.secondary.tracing.open_telemetry.main import tracer
 
 logger = logging.getLogger(__name__)
 
+
 class Order_Controller:
-    def __init__(self, app, create_order_use_case):
+    def __init__(self, app: FastAPI, create_order_use_case: Create_Order):
         self.app = app
         self.create_order_use_case = create_order_use_case
 
+ 
         @app.post("/orders", status_code=status.HTTP_201_CREATED)
         def create_order(order: Order_Creation):
-               try:
-                   order = self.create_order_use_case.execute('user_id', order.product_id)
-                   return order
-               except Exception as error:
+            try:
+                order = self.create_order_use_case.execute('user_id', order.product_id)
+                return order
+            except Exception as error:
                    logger.exception(error)
--- src/adapters/primary/http/fast_api/main.py
+++ src/adapters/primary/http/fast_api/main.py
@@ -1,8 +1,7 @@
-
 from fastapi import FastAPI
 from src.adapters.primary.http.fast_api.controllers.order_controller import Order_Controller
 from src.use_cases.order.create_order.main import Create_Order
-
+from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
 
 class FastApiManager:
     def __init__(self, create_order_use_case: Create_Order):
@@ -11,9 +10,11 @@ class FastApiManager:
     def initialize(self):
         app = FastAPI()
         
+        FastAPIInstrumentor.instrument_app(app)
+        
         @app.get("/ping")
         def ping():
-            return {"Status": "alive"}
+                return {"Status": "alive"}
         
         Order_Controller(app, self.create_order_use_case)
       
